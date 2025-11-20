4f0a98e722e2fb2a8d9516b5ece113e2f0596754
temmie_950807 <kapoo950807@gmail.com>
2025-11-20 16:59:26 +0800

    Update tile dimensions and enhance map generation logic

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import random
 from turtle import *
 import time
 
-TILE_HEIGHT = 4
-TILE_WIDTH = 1
+TILE_HEIGHT = 5
+TILE_WIDTH = 5
 BLOCK = [
     # 直線
     [(0, 0), (1, 0)],
@@ -24,21 +24,18 @@ BLOCK = [
     [(0, 0), (1, 0), (2, 0), (2, 1)],
 ]
 
-GAME_MAP_HEIGHT = TILE_HEIGHT*3
-GAME_MAP_WIDTH = TILE_WIDTH*3
-
 SCREEN_WIDTH = 640
 SCREEN_HEIGHT = 480
 
 # LEFT, UP, RIGHT, DOWN
 DX = [-1, 0, 1, 0]
 DY = [0, -1, 0, 1]
-MAP_GAP = 16
-TILE_GAP = 48
+MAP_CELL_GAP = 16
 
 class TileTable:
     def __init__(self, height = TILE_HEIGHT, width = TILE_WIDTH):
         self.table = [[0] * width for _ in range(height)]
+        self.generate_map()
 
     @property
     def height(self):
@@ -58,10 +55,13 @@ class TileTable:
         return 0<=nowY and nowY<self.height and 0<=nowX and nowX<self.width
     
     def print(self):
+        print("Tile Table:")
         for row in self.table:
             for cell in row:
                 print(f"{cell:2}", end=" ")
             print()
+        print("hegiht:", self.height, "width:", self.width)
+        print()
 
     def generate_map(self, nowY = 0, nowX = 0, id = 1):
         if nowY==self.height and nowX==0:
@@ -91,155 +91,185 @@ class TileTable:
 
 class GameMap:
     def __init__(self, table: TileTable):
-        tracer(0, delay=None)
-        speed(0)
-        hideturtle()
         self.table = table
         for idx in range(self.table.height):
             self.table[idx] = self.table[idx][-1:0:-1]+self.table[idx]
-        self.table.print()
 
-    def position(self, y, x):
-        """
-        給定 table 的 (y, x) 座標，回傳該格左上角的畫布座標 (screen_y, screen_x)
-        """
-        return ((y-self.table.height*3/2)*MAP_GAP+SCREEN_HEIGHT/2, (x-self.table.width*3/2)*MAP_GAP+SCREEN_WIDTH/2)
+        self.gameTable = [
+            [0 for _ in range(self.table.width*3+5)] for __ in range(self.table.height*3+5)
+        ]
+
+        # 左右的外牆
+        for i in range(self.height):
+            self.gameTable[i][0] = 1
+            self.gameTable[i][1] = 1
+            self.gameTable[i][-1] = 1
+            self.gameTable[i][-2] = 1
+
+        # 上下的外牆
+        for j in range(self.width):
+            self.gameTable[0][j] = 1
+            self.gameTable[1][j] = 1
+            self.gameTable[-1][j] = 1
+            self.gameTable[-2][j] = 1
+
+        # 中間的地圖
+        for i in range(self.table.height):
+            for j in range(self.table.width):
+                self.fill(i, j, i, j)
+                for k in range(4):
+                    if (self.table.is_inside(i+DY[k], j+DX[k]) and
+                        self.table[i][j]==self.table[i+DY[k]][j+DX[k]]):
+                        self.fill(i, j, i+DY[k], j+DX[k])
+
+    @property
+    def height(self):
+        return len(self.gameTable)
     
-    def draw_line(self, y1, x1, y2, x2):
-        """
-        給定 table 的 (y1, x1) 跟 (y2, x2)，畫出連線
-        """
-        y1, x1 = self.position(y1, x1)
-        y2, x2 = self.position(y2, x2)
-        teleport(x1, y1)
-        goto(x2, y2)
-
-    # 給定網格的 (y, x)，畫出該格的邊界
-    def draw_border(self, y, x, dir):
-        """
-        給定網格的 (y, x) 和方向 dir，畫出該格的邊界
-        0: LEFT
-        1: UP
-        2: RIGHT
-        3: DOWN
-        """
-        match dir:
-            case 0: # LEFT
-                self.draw_line(y, x, y+1, x)
-            case 1: # UP
-                self.draw_line(y, x, y, x+1)
-            case 2: # RIGHT
-                self.draw_line(y, x+1, y+1, x+1)
-            case 3: # DOWN
-                self.draw_line(y+1, x, y+1, x+1)
-
-    # 給定 TILE 的座標左上角 (y1, x1) 跟右下角 (y2, x2)，在裡面畫出矩形
-    def draw_inner_wall(self, y1, x1, y2, x2):
-        """
-        給定 table 的座標左上角 (y1, x1) 跟右下角 (y2, x2)，在裡面畫出矩形
-        """
-
-        if ((y1, x2)==(y2, x2)):
-            y1 = y1*3+0.7
-            x1 = x1*3+0.7
-            y2 = y2*3+2.3
-            x2 = x2*3+2.3
-        else:
-            dy1 = [0.7, 1.5, 0.7, 1.5]
-            dx1 = [1.5, 0.7, 1.5, 0.7]
-            dy2 = [2.3, 1.5, 2.3, 1.5]
-            dx2 = [1.5, 2.3, 1.5, 2.3]
-            for i in range(4):
-                if ((y1, x1)==(y2+DY[i], x2+DX[i])):
-                    y1 = y1*3+dy1[(i+2)%4]
-                    x1 = x1*3+dx1[(i+2)%4]
-                    y2 = y2*3+dy2[(i+2)%4]
-                    x2 = x2*3+dx2[(i+2)%4]
-                    break
+    @property
+    def width(self):
+        return len(self.gameTable[0])
     
-        y1, x1 = self.position(y1, x1)
-        y2, x2 = self.position(y2, x2)
+    def __getitem__(self, index):
+        return self.gameTable[index]
+    
+    def __setitem__(self, index, value):
+        self.gameTable[index] = value
 
+    def fill(self, tileY1, tileX1, tileY2, tileX2):
+        tileY1, tileX1, tileY2, tileX2 = min(tileY1, tileY2), min(tileX1, tileX2), max(tileY1, tileY2), max(tileX1, tileX2)
+        tileY1 = tileY1*3+3
+        tileX1 = tileX1*3+3
+        tileY2 = tileY2*3+3+1
+        tileX2 = tileX2*3+3+1
+        for i in range(tileY1, tileY2+1):
+            for j in range(tileX1, tileX2+1):
+                self.gameTable[i][j] = 1
+    
+    def print(self):
+        print("Game Map:")
+        for row in self.gameTable:
+            for cell in row:
+                print(f"{cell:2}", end=" ")
+            print()
+        print("hegiht:", self.height, "width:", self.width)
+        print()
+
+class Canva:
+    def __init__(self, gameTable: GameMap):
+        tracer(0, delay=None)
+        speed(0)
+        hideturtle()
+        self.gameTable = gameTable
+
+        self._draw_axis()
+
+        fillcolor("#0000FF")
+        for i in range(self.gameTable.height-1):
+            for j in range(self.gameTable.width-1):
+                if self.gameTable[i][j] and self.gameTable[i+1][j] and self.gameTable[i][j+1] and self.gameTable[i+1][j+1]:
+                    # 整個正方形
+                    self._draw_rectangle(i, j, i+1, j+1)
+                elif self.gameTable[i][j] and self.gameTable[i][j+1]:
+                    # 向右的長方形
+                    self._draw_rectangle(i, j, i, j+1)
+                elif self.gameTable[i][j] and self.gameTable[i+1][j]:
+                    # 向下的長方形
+                    self._draw_rectangle(i, j, i+1, j)
+
+        self._draw_table()
+        update()
+
+    def _position(self, mapY, mapX):
+        """
+        給定 table 的 (mapY, mapX) 座標，回傳該格左上角的畫布座標 (screen_y, screen_x)
+        """
+        screenY = (mapY - (self.gameTable.height-1)/2) * MAP_CELL_GAP + SCREEN_HEIGHT/2 # 為什麼要 -1，不清楚
+        screenX = (mapX - (self.gameTable.width-1)/2) * MAP_CELL_GAP + SCREEN_WIDTH/2
+        return (screenY, screenX)
+    
+    def _draw_line(self, mapY1, mapX1, mapY2, mapX2):
+        """
+        給定 table 的 (mapY1, mapX1) 跟 (mapY2, mapX2)，畫出連線
+        """
+        screenY1, screenX1 = self._position(mapY1, mapX1)
+        screenY2, screenX2 = self._position(mapY2, mapX2)
+        teleport(screenX1, screenY1)
+        goto(screenX2, screenY2)
+
+    def _draw_rectangle(self, mapY1, mapX1, mapY2, mapX2):
+        """
+        給定 table 的對角線座標 (mapY1, mapX1) 跟 (mapY2, mapX2)，在裡面畫出矩形
+        """
+        mapY1, mapX1, mapY2, mapX2 = min(mapY1, mapY2), min(mapX1, mapX2), max(mapY1, mapY2), max(mapX1, mapX2)
+        mapY1 -= 0.4
+        mapX1 -= 0.4
+        mapY2 += 0.4
+        mapX2 += 0.4
+        screenY1, screenX1 = self._position(mapY1, mapX1)
+        screenY2, screenX2 = self._position(mapY2, mapX2)
         penup()
-        teleport(x1, y1)
+        teleport(screenX1, screenY1)
         begin_fill()
-        goto(x1, y2)
-        goto(x2, y2)
-        goto(x2, y1)
-        goto(x1, y1)
+        goto(screenX1, screenY2)
+        goto(screenX2, screenY2)
+        goto(screenX2, screenY1)
+        goto(screenX1, screenY1)
         end_fill()
         pendown()
 
-    def draw_table(self):
+    def _draw_border(self, mapY, mapX, dir):
         """
-        畫灰色網格
+        給定 table 的 (mapY, mapX) 和方向 dir，畫出該格的邊界
+        0: RIGHT
+        1: DOWN
+        """
+        if dir==0:
+            self._draw_line(mapY, mapX, mapY, mapX+1)
+        else:
+            self._draw_line(mapY, mapX, mapY+1, mapX)
+
+    def _draw_table(self):
+        """
+        畫灰色座標網格
         """
         pencolor("#CCCCCC")
         pensize(1)
-        for i in range(self.table.height*3):
-            for j in range(self.table.width*3):
-                for k in range(4):
-                    self.draw_border(i, j, k)
-        update()
+        for i in range(0, self.gameTable.height):
+            for j in range(0, self.gameTable.width):
+                if i<self.gameTable.height-1 and j<self.gameTable.width-1:
+                    self._draw_border(i, j, 0)  # RIGHT
+                    self._draw_border(i, j, 1)  # DOWN
+                elif i==self.gameTable.height-1 and j<self.gameTable.width-1:
+                    self._draw_border(i, j, 0)  # RIGHT
+                elif i<self.gameTable.height-1 and j==self.gameTable.width-1:
+                    self._draw_border(i, j, 1)  # DOWN
 
-    def draw_axis(self):
+    def _draw_axis(self):
         """
         畫座標軸
         """
         pensize(3)
         pencolor("#FF0000")
-        teleport(-1000, 0)
-        goto(1000, 0)
+        teleport(-1000, SCREEN_HEIGHT/2)
+        goto(1000, SCREEN_HEIGHT/2)
         pencolor("#00FF00")
-        teleport(0, -1000)
-        goto(0, 1000)
-        update()
+        teleport(SCREEN_WIDTH/2, -1000)
+        goto(SCREEN_WIDTH/2, 1000)
 
-    def draw_inner_border(self):
-        """
-        畫內部邊界
-        """
-        pencolor("#0000FF")
-        fillcolor("#0000FF")
-        for i in range(0, self.table.height):
-            for j in range(0, self.table.width):
-                fillcolor("#0000FF")
-                self.draw_inner_wall(i, j, i, j)
-                for k in range(4):
-                    if self.table.is_inside(i+DY[k], j+DX[k]) and self.table[i][j]==self.table[i+DY[k]][j+DX[k]]:
-                        self.draw_inner_wall(i, j, i+DY[k], j+DX[k])
-        update()
-
-    def draw_outside_border(self):
-        """
-        畫外邊框
-        """
-        pensize(5)
-        fillcolor("#0000FF")
-        for i in range(-1, self.table.height):
-            self.draw_inner_wall(i, -1, i+1, -1)
-            self.draw_inner_wall(i, self.table.width, i+1, self.table.width)
-        for i in range(-1, self.table.width):
-            self.draw_inner_wall(-1, i, -1, i+1)
-            self.draw_inner_wall(self.table.height, i, self.table.height, i+1)
-        update()
+        pensize(1)
 
 if __name__ == "__main__":
-    while True:
-        screen = Screen()
-        screen.setup(width=SCREEN_WIDTH, height=SCREEN_HEIGHT)
-        # (左下角x, 左下角y, 右上角x, 右上角y)
-        screen.setworldcoordinates(0, SCREEN_HEIGHT, SCREEN_WIDTH, 0)
+    screen = Screen()
+    screen.setup(width=SCREEN_WIDTH, height=SCREEN_HEIGHT)
+    screen.setworldcoordinates(0, SCREEN_HEIGHT, SCREEN_WIDTH, 0) # (左下角x, 左下角y, 右上角x, 右上角y)
 
-        tileTable = TileTable()
-        tileTable.generate_map()
-        gameMap = GameMap(tileTable)
-        gameMap.draw_axis()
-        gameMap.draw_table()
-        gameMap.draw_outside_border()
-        gameMap.draw_inner_border()
-        break
-        time.sleep(1)
-        reset()
+    tileTable = TileTable()
+    tileTable.print()
+
+    gameMap = GameMap(tileTable)
+    gameMap.print()
+
+    canva = Canva(gameMap)
 
 input()
